d462edb6ac10e4b7162b2cb24e8dd3157a550208
onkelhoy <onkelhoy@gmail.com>
2024-04-16 18:03:49 +0200

    add: datasets added to experiment on

--- Code/Asset/Datasets/timelapse/build_dataset.py
+++ Code/Asset/Datasets/timelapse/build_dataset.py
@@ -1,6 +1,8 @@
 import cv2
 import sys
 import os
+import math 
+import json
 
 current_script_dir = os.path.dirname(os.path.abspath(__file__))
 
@@ -33,30 +35,58 @@ def run_video_to_frames():
 
 # this function would group frames into smaller groups based on threshold,
 # this means, if set contains 100 images and threshold is 10%, we can expect 10 groups containing 10 frames each 
-def group_frames(relative_directory, threshold):
+def group_frames(directory, threshold):
   frameindex = 0
 
-  directory = os.path.normpath(os.path.join(current_script_dir, relative_directory))
-
   dataset = {
-    paths: [],
-    results: {}
+    'paths': [],
+    'groups': {}
   }
 
-  firstgroupindex = 0
+  entries = os.listdir(directory)
+  size = len(entries)
 
-  for entry in os.listdir(directory):
+  poolsize = max(1, math.floor(size * threshold))
+  number_of_pools = math.ceil(size / poolsize)
+
+  for entry in entries:
       # Construct the full path to the entry
       full_path = os.path.join(directory, entry)
 
       if os.path.isfile(full_path):
+          dataset['paths'].append(full_path)
+          poolindex = frameindex % number_of_pools
 
-          dataset.paths.append(full_path)
-          dataset.results[full_path] = 
+          if poolindex not in dataset['groups']:
+            dataset['groups'][poolindex] = []
+
+          dataset['groups'][poolindex].append(full_path)
+
           frameindex += 1
 
-def main():
-  group_frames("multiple-timelapse/00", 1)  
+  return dataset
 
+def build_multiple_timelapse_set(threshold = 0.1, name = "high"):
+  directory = os.path.normpath(os.path.join(current_script_dir, "multiple-timelapse"))
+  output_json = os.path.join(directory, f"{name}fit.dataset.json")
+
+  sets = {}
+
+  for entry in os.listdir(directory):
+    full_path = os.path.join(directory, entry)
+
+    if os.path.isdir(full_path):
+      sets[entry] = group_frames(full_path, threshold)
+
+  with open(output_json, 'w') as file:
+    json.dump(sets, file)
+
+
+def main():
+  # build high fit dataset 
+  build_multiple_timelapse_set(0.06, "high")
+
+  # build low fit dataset 
+  build_multiple_timelapse_set(0.25, "low")
 
 main()
